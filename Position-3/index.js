4a4c289240aa62c766ff74285d58de7953d76737
songmiO <dhthd1060@gmail.com>
2022-08-25 00:02:10 +0900

    Add : Add Modal Window Open Feature

--- Position-3/index.js
+++ Position-3/index.js
@@ -1,1 +1,11 @@
-1. 구독버튼클릭 , 모달창
+const modal = document.getElementsByClassName('modal')[0];
+const openModalBtn = document.getElementsByClassName('subscribe')[0];
+const closeModalBtn = document.getElementsByClassName('close-button')[0];
+
+openModalBtn.addEventListener('click', (e) => {
+  modal.style.display = 'block';
+});
+
+closeModalBtn.addEventListener('click', (e) => {
+  document.getElementsByClassName('modal')[0].style.display = 'none';
+});
