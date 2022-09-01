2c3ef625507006c19adabc695fa93ab6dea3fe54
songmiO <dhthd1060@gmail.com>
2022-09-01 18:51:09 +0900

    Modify : Modify modal event code

--- Position-3/index.js
+++ Position-3/index.js
@@ -2,10 +2,17 @@ const modal = document.getElementsByClassName('modal')[0];
 const openModalBtn = document.getElementsByClassName('subscribe')[0];
 const closeModalBtn = document.getElementsByClassName('close-button')[0];
 
-openModalBtn.addEventListener('click', (e) => {
-  modal.style.display = 'block';
+// 1. style.display 에 접근하여 모달 띄우기
+// openModalBtn.addEventListener('click', (e) => {
+//   modal.style.display = 'block';
+// });
+
+// 2. show-modal 클래스를 추가하여 모달 띄우기
+openModalBtn.addEventListener('click', () => {
+  modal.classList.add('show-modal');
 });
 
-closeModalBtn.addEventListener('click', (e) => {
-  document.getElementsByClassName('modal')[0].style.display = 'none';
+// close-button 클릭 시 창 닫기
+closeModalBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
 });
